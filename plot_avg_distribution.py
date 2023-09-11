c2bf8d9a5928dd52801e7462ebe5f31e8289288d
sahithyp@gmail.com <sahithyp@gmail.com>
2023-09-11 13:11:52 -0400

    commit computing avg in plot_avg_distribution.py

--- plot_avg_distribution.py
+++ plot_avg_distribution.py
@@ -26,36 +26,65 @@ def initialize_game(costs, n, m):
     return game
 
 def plot_avg_dist(iterations):
-    costs = [0.0001]
+    costs = [0.0001, 0.05]
+    number_firms = len(costs)
     total_distr = {}
+    max_surp = 0
+    min_surp = np.infty
+    number_locations = 3
 
-    for i in range(len(costs)):
+    for i in range(number_firms):
         total_distr[i] = []
 
-    for i in range(iterations):
-        game = initialize_game(costs, len(costs), 5)
+    for iter in range(iterations):
+        game = initialize_game(costs, len(costs), number_locations)
         game.solve_grad_ascent(0.01, 0.00000001, 100000)
         sing_distr = game.demand_plot(costs)
-        sing_dict = {}
-        for pair in sing_distr[0]:
-            sing_dict[pair[0]] = pair[1]
-        lists = sorted(sing_dict.items())
-        x, y = zip(*lists)
-        plt.scatter(x, y)
-        plt.plot(x, y, label="iter: %d" % i)
+        # sing_dict = {}
+        # print(sing_distr)
+        # for pair in sing_distr[0]:
+        #     sing_dict[pair[0]] = pair[1]
+        # lists = sorted(sing_dict.items())
+        # x, y = zip(*lists)
+        # plt.scatter(x, y)
+        # plt.plot(x, y, label="iter: %d" % iter)
 
-        for c in range(len(costs)):
-            for firm in sing_distr:
-                if firm == c:
-                    total_distr[c].extend(sing_distr[firm])
+        for i in range(number_firms):
+            max_in_sing_distr = max(sing_distr[i])[0]
+            min_in_sing_distr = min(sing_distr[i])[0]
+            if max_in_sing_distr > max_surp:
+                max_surp = max_in_sing_distr
+            if min_in_sing_distr < min_surp:
+                min_surp = min_in_sing_distr
 
-    for i in range(len(costs)):
+            total_distr[i].append({("iter %d" % iter): sing_distr[i]})
+
+        # for i in range(number_firms):
+        #     for firm in sing_distr:
+        #         if firm == i:
+        #             total_distr[i].extend(sing_distr[firm])
+    print(total_distr)
+
+    for i in total_distr:
+        surp_range = max_surp - min_surp
+        increment = surp_range / (number_locations * 2)
+        first = min_surp + increment
+        increments = [first]
+        for num in range(1,number_locations*2):
+            increments.append(increments[num-1] + increment)
+
+        for increment in increments:
+            surp_near_increment = []
+            for iter in iterations:
+                surp_vals = iter
+                surp = min(iter, key=lambda x:abs(x-increment))
+
         tot_surp_perc_ai = {}
         for firm_key in total_distr:
             if firm_key == i:
                 for pair in total_distr[firm_key]:
                     tot_surp_perc_ai[pair[0]] = pair[1]
-
+        # print("surp", tot_surp_perc_ai)
         lists = sorted(tot_surp_perc_ai.items())
         x,y = zip(*lists)
         plt.scatter(x,y)
@@ -67,8 +96,16 @@ def plot_avg_dist(iterations):
     plt.legend()
     plt.show()
 
-    return total_distr
+    # return total_distr
 
 
+'''
+from iter1: surp to perc_ai
+from iter2: surp to perc_ai
+from iter3: surp to perc_ai
 
-
+create increments: divide range(surp) by 2 x number locations
+iterate through each list:
+    get perc_ai value linked to surplus closest to each increment
+    add to new dictionary to plot final graph
+'''
